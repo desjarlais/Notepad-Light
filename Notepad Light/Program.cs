6d13f6d4df17b28e956c7612dd009dfbfdaf23e3
Brandon Desjarlais <brandondesjarlais@outlook.com>
2026-04-07 16:23:10 -0500

    adding ability for settings to persist across updates

--- Notepad Light/Program.cs
+++ Notepad Light/Program.cs
@@ -5,6 +5,14 @@ namespace Notepad_Light
         [STAThread]
         static void Main()
         {
+            // migrate user settings from a previous version after an upgrade
+            if (Properties.Settings.Default.UpgradeRequired)
+            {
+                Properties.Settings.Default.Upgrade();
+                Properties.Settings.Default.UpgradeRequired = false;
+                Properties.Settings.Default.Save();
+            }
+
             Application.EnableVisualStyles();
             ApplicationConfiguration.Initialize();
             Application.Run(new FrmMain());
--- Notepad Light/Properties/Settings.Designer.cs
+++ Notepad Light/Properties/Settings.Designer.cs
@@ -300,5 +300,17 @@ namespace Notepad_Light.Properties {
                 this["SpellCheckLanguage"] = value;
             }
         }
+
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool UpgradeRequired {
+            get {
+                return ((bool)(this["UpgradeRequired"]));
+            }
+            set {
+                this["UpgradeRequired"] = value;
+            }
+        }
     }
 }
